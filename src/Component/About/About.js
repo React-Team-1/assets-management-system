8bb48a5c9108dc3192ff5997bf0958a5ea2e9ede
aminhad <amin.hadi@amalitech.org>
2021-05-17 16:47:04 +0000

    Backend component added

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,39 +1,47 @@
 import React from "react"
 import FrontEndTeam from "./Frontend"
-import FrontEndTeamInfo from "./FrontendTeam"
+import TeamInfo from "./TeamMembers"
 
 
 
-
-const About = ()=>{
-    return(
+const About = () => {
+    return (
         <div className="About">
-         
-         <img  id="background-imgStyle" src="Assets/backgrounddStyle.png" alt="backgroundStyle"/>
-         <img  id="background-imgStyle2" src="Assets/backgrounddStyle.png" alt="backgroundStyle"/>
-          {/* Front end display starts here */}
-          <div>
-          <h2 id="front-end-title">FrontEnd Teams</h2>
-            <div className="front-end-Wrapper">
-            {FrontEndTeamInfo.Frontend.map(teams)}
+
+            <img id="background-imgStyle" src="Assets/backgrounddStyle.png" alt="backgroundStyle" />
+            <img id="background-imgStyle2" src="Assets/backgrounddStyle.png" alt="backgroundStyle" />
+            {/* Front end display starts here */}
+            <div>
+                <h2 id="front-end-title">FrontEnd Teams</h2>
+                <div className="front-end-Wrapper">
+                    {TeamInfo.Frontend.map(teams)}
+                </div>
             </div>
-          </div>
-             
-           
+
+            <div>
+                <h2 id="front-end-title">BackEnd Teams</h2>
+                <div className="front-end-Wrapper">
+                    {TeamInfo.Backend.map(teams)}
+                </div>
+            </div>
+            
+            <div className="footer">
+                <p>Copyright 2021</p>
+            </div>
         </div>
     )
 }
 
 
-function teams (member){
+function teams(member) {
 
     return (
         <FrontEndTeam
-        key= {member.teamId}
-        teamName = {member.pName}
-        personImage ={member.pImage}
-        track={member.track}>
-        
+            key={member.teamId}
+            teamName={member.pName}
+            personImage={member.pImage}
+            track={member.track}>
+
 
         </FrontEndTeam>
 
---
+++ src/Component/About/Backend.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+
+
+const backEndTeam =(props)=>{
+  let element = <FontAwesomeIcon icon={faEnvelope} />
+  let element2 = <FontAwesomeIcon icon={faLinkedinIn} />
+  return(
+          <div  className="front-end-Teams-div">
+          <img src={props.personImage}  alt="Amin"/> 
+              <div className="details">
+              <h3>{props.teamName}</h3>
+              <h4 >{props.track}</h4>
+              <div className="connections-div">
+                <p className="fontIcons">{element2}</p>
+                <p className="fontIcons" id="font-second">{element}</p>
+              </div>
+              </div>
+          </div>
+  )
+}
+
+
+export default backEndTeam;
