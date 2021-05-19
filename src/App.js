19c9a7688cec701d6a43e8091c240dac1f483ae2
unknown <jedidiah.eyiah-king@amalitech.org>
2021-05-19 04:11:31 -0700

    Navbar and overview added to the about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import React from "react"
+/*import { Router } from "react-router";*/
 import './App.css';
 import About from "./Component/About/About"
 
+
 function App() {
   return (
+
+    
     <div className="App">
+      
+      
      <About />
     </div>
   );
--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,20 +2,39 @@ import React from "react"
 import FrontEndTeam from "./Frontend"
 import Group from "./Group"
 import TeamInfo from "./TeamMembers"
+import Navbar from "./Navbar/Navbar"; 
+import { BrowserRouter as Router} from 'react-router-dom';
+import Card from "../Card/card.js";
+
+
 
 
 
 const About = () => {
     return (
+        
+        
         <div className="About">
+        <Router>
+      <Navbar/>
+      </Router>
+
+
 
             <img id="background-imgStyle" src="Assets/backgrounddStyle.png" alt="backgroundStyle" />
             <img id="background-imgStyle2" src="Assets/backgrounddStyle.png" alt="backgroundStyle" />
+
+            <Card title="Overview"
+                textBody="Amalitech Asset Management System 
+            is a dynamic system that is used 
+            in the management and record keeping 
+            of all items that exits in the organization"
+             />
             
             <Group />
 
             {/* Front end display starts here */}
-            <div>
+            <div className="Team-divs-style">
                 <h2 id="front-end-title">FrontEnd Teams</h2>
                 <div className="front-end-Wrapper">
                     {TeamInfo.Frontend.map(teams)}
