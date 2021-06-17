fd3c44af7fb3566fe9ef4fbbf599e01b30e1519a
aminhad <amin.hadi@amalitech.org>
2021-06-17 15:56:58 +0000

    Displaying of selected image fixed

--- src/Component/UploadItems/NewItem/AddNewItem.js
+++ src/Component/UploadItems/NewItem/AddNewItem.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
-// import { Field } from 'formik'
+// import { Form, Formik } from 'formik'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import InputField from './InputField'
 import './AddNewItem.css'
 
 
-export default class AddNewItem extends Component {
+class AddNewItem extends Component {
+
     state = {
+        assetInfo:[]
+    };
+  
 
-        selectedFile: <FontAwesomeIcon icon={faImage} id="imgIcon" />
-    }
+    fileSelectedhandler = () => {
+        let customFile = document.getElementById("customFile")
+        let ItemImg = document.getElementById("ItemImg")
+        let imgIcon = document.getElementById("imgIcon")
+        
 
-    fileSelectedhandler = event => {
-        const customFile = document.getElementById('customFile')
-        this.state = customFile.files;
+        const [file] = customFile.files
+        console.log(file);
+        if (file) {
+            ItemImg.style.display = 'unset'
+            imgIcon.style.display = 'none'
+            ItemImg.src = URL.createObjectURL(file)
+           this.url = URL.createObjectURL(file);
+        }
     }
 
     fileUploaderHandler = () => {
@@ -22,60 +34,89 @@ export default class AddNewItem extends Component {
         document.getElementById("customFile").click();
     }
 
-
-
-
-
     render() {
 
-        let imgIcon = this.state.selectedFile;
+        let imgIcon = <FontAwesomeIcon icon={faImage} id="imgIcon" />;
 
         return (
             <div>
-                <div className="pic-container" id="fileContainer" >
-                    <input
-                        type="file"
-                        id="customFile"
-                        onChange={this.fileSelectedhandler}
-                        accept="image/*"
 
-                    />
-                    <button id="OpenImgUpload" onClick={this.fileUploaderHandler} >{imgIcon}<span>Upload Picture</span></button>
-                </div>
-                <div className="input-container">
-                    <InputField title="Asset Tag" />
-                    <InputField title="Item type" />
-                    <InputField title="Location" />
-                    <InputField title="Area" />
-                    <InputField title="Serial Number" />
-                    <InputField title="Brand" />
-                    <InputField title="Status" />
-                    <InputField title="Ownership" />
-                    <InputField title="Category" />
-                    <InputField title="Date Purchased" />
-                    <InputField title="In-Charge" />
-                    <InputField title="Price" />
-                </div>
-                <div className="textarea-container">
-                    <div>
-                        <label>Description</label>
-                        <textarea rows="5" cols="33" spellCheck="true" />
-                    </div>
-                    <div>
-                        <label>Remarks</label>
-                        <textarea rows="5" cols="33" spellCheck="true" />
-                    </div>
+            
+                    <form >
+                        <div className="pic-container" id="fileContainer" >
+                            <input
+                                type="file"
+                                id="customFile"
+                                name="customFile"
+                                onChange={this.fileSelectedhandler}
+                                accept="image/*"
+                            />
+                            <label id="OpenImgUpload" onClick={this.fileUploaderHandler}>
+                                <img src="#" alt="" id="ItemImg" />
+                                {imgIcon}
+                            </label>
 
-                </div>
-                <div className="buttons">
-                    <button>Close</button>
-                    <button>Add Item</button>
-                </div>
+                        </div>
+                        <div className="input-container">
+                            <InputField title="Asset Tag" />
+                            <InputField title="Item type" />
+                            <InputField title="Location" />
+                            <InputField title="Area" />
+                            <InputField title="Serial Number" />
+                            <InputField title="Brand" />
+                            <InputField title="Status" />
+                            <InputField title="Ownership" />
+                            <InputField title="Category" />
+                            <InputField title="Date Purchased" />
+                            <InputField title="In-Charge" />
+                            <InputField title="Price" />
+                        </div>
+                        <div className="textarea-container">
+                            <div>
+                                <label>Description</label>
+                                <textarea rows="5" cols="33" spellCheck="true" />
+                            </div>
+                            <div>
+                                <label>Remarks</label>
+                                <textarea rows="5" cols="33" spellCheck="true" />
+                            </div>
+
+                        </div>
+                        <div className="buttons">
+                            <button>Close</button>
+                            <button type="submit" onClick={this.handleSubmit}>Add Item</button>
+                        </div>
+
+                    </form>
 
 
             </div>
 
         )
     }
+
+
+
+    handleSubmit(e){
+        e.preventDefault()
+        alert("I have clicked");
+        const val = document.querySelectorAll(".input-div input");
+         let item = []
+        val.forEach((input)=>{
+            item.push(input.value)
+            //  this.state.assetInfo.push(input.value);
+            // this.setState({
+            //     assetInfo: 
+            // })
+        })
+        console.log(item);
+        // this.setState({
+        //     assetInfo: item
+        // })
+        console.log(this.state);
+
+       
+    }
 }
+export default AddNewItem;
 
